8419e2eed84d473a96c4dc0c6abf215b669e4fe4
kalifun <kalifun@163.com>
2026-04-23 11:03:45 +0800

    fix: improve rime-ice installation error handling and ensure custom YAML file is always activated

--- home/kalifun/default.nix
+++ home/kalifun/default.nix
@@ -36,11 +36,20 @@
       trap 'rm -rf "$TMP_DIR"' EXIT
 
       echo "Installing rime-ice into $RIME_DIR"
-      ${pkgs.git}/bin/git clone --depth=1 https://github.com/iDvel/rime-ice "$TMP_DIR/rime-ice"
-      cp -R "$TMP_DIR/rime-ice"/. "$RIME_DIR"/
+      if ${pkgs.git}/bin/git clone --depth=1 https://github.com/iDvel/rime-ice "$TMP_DIR/rime-ice" \
+        && cp -R "$TMP_DIR/rime-ice"/. "$RIME_DIR"/; then
+        :
+      else
+        echo "home-manager: warning: rime-ice bootstrap failed (network?). Fix and run darwin-rebuild again." >&2
+      fi
     fi
+  '';
 
-    # 放在雾凇安装之后，避免与 home.file 激活顺序竞态导致 ~/Library/Rime 里没有该文件
+  # 必须在 linkGeneration 之后：activate 里 set -e，若上面 git clone 失败会整段退出，原来写 squirrel 的 install 永远执行不到。
+  # 单独挂在 linkGeneration 后，保证每次激活都会落盘，且晚于 HM 清理/建链。
+  home.activation.syncRimeSquirrelCustom = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    RIME_DIR="$HOME/Library/Rime"
+    ${pkgs.coreutils}/bin/mkdir -p "$RIME_DIR"
     ${pkgs.coreutils}/bin/install -Dm644 ${../../config/rime/squirrel.custom.yaml} "$RIME_DIR/squirrel.custom.yaml"
   '';
 
